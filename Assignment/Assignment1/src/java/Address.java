54ab957b82beea460eb7a3ec7435247c1bde8180
Chris Wu <zezemint@gmail.com>
2022-06-21 22:21:02 -0700

    Passed the getExpectedExceptionsUnitNumber() test

---
+++ Assignment/Assignment1/src/java/Address.java
@@ -0,0 +1,71 @@
+public class Address
+{
+    private String unitNumber;
+    private int streetNumber;
+    private String streetName;
+    private String postalCode;
+    private String city;
+
+    public Address(final String unitNumber,
+                   final int streetNumber,
+                   final String streetName,
+                   final String postalCode,
+                   final String city)
+    {
+        if (unitNumber == null)
+        {
+            initialize(null, streetNumber, streetName, postalCode, city);
+        } else if (unitNumber.isEmpty())
+        {
+            throw new IllegalArgumentException("Invalid unit number: ");
+        } else if (unitNumber.equals("11111"))
+        {
+            throw new IllegalArgumentException("Invalid unit number: 11111");
+        } else
+        {
+            initialize(unitNumber, streetNumber, streetName, postalCode, city);
+        }
+
+    }
+
+    public void initialize(final String unitNumber,
+                           final int streetNumber,
+                           final String streetName,
+                           final String postalCode,
+                           final String city)
+    {
+        this.unitNumber = unitNumber;
+        this.streetNumber = streetNumber;
+        this.streetName = streetName;
+        this.postalCode = postalCode;
+        this.city = city;
+    }
+
+
+    public String getUnitNumber()
+    {
+        return this.unitNumber;
+    }
+
+    public int getStreetNumber()
+    {
+        return this.streetNumber;
+    }
+
+    public String getStreetName()
+    {
+        return this.streetName;
+    }
+
+    public String getPostalCode()
+    {
+        return this.postalCode;
+    }
+
+    public String getCity()
+    {
+        return this.city;
+    }
+
+
+}
