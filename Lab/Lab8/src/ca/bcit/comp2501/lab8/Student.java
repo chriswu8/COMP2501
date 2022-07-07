8263bb491ef98cdf5617b02aea2f2f55eda64098
Chris Wu <zezemint@gmail.com>
2022-07-07 16:25:02 -0700

    added Javadocs

--- Lab/Lab8/src/ca/bcit/comp2501/lab8/Student.java
+++ Lab/Lab8/src/ca/bcit/comp2501/lab8/Student.java
@@ -25,33 +25,185 @@ public class Student
     {
         Scanner myScanner;
         myScanner = new Scanner(System.in);
+        final String  firstName;
+        final String  lastName;
+        final int     id;
+        final int     birthYear;
+        final double  percentageGrade;
+        final boolean pass;
 
         System.out.println("What is the student's first name? ");
-        this.firstName = myScanner.next();
+        firstName = checkName(myScanner.next());
+        this.firstName = firstName;
 
         System.out.println("What is the student's last name? ");
-        this.lastName = myScanner.next();
+        lastName = checkName(myScanner.next());
+        this.lastName = lastName;
 
-        System.out.println("What is the student's id? ");
-        this.id = myScanner.nextInt();
+        System.out.println("What is the student's id number? ");
+        id = checkDigits(myScanner.next());
+        this.id = id;
 
-        System.out.println("What is the student's last year of birth? ");
-        this.birthYear = myScanner.nextInt();
+        System.out.println("What is the student's year of birth? ");
+        birthYear = checkBirthYear(myScanner.next());
+        this.birthYear = birthYear;
 
         System.out.println("What is the student's grade in percentage? ");
-        this.percentageGrade = myScanner.nextDouble();
+        percentageGrade = checkGrade(myScanner.next());
+        this.percentageGrade = percentageGrade;
 
-        if(percentageGrade >= PASSING_GRADE)
-        {
-            this.pass = true;
-        }
-        else
-        {
-            this.pass = false;
-        }
+        pass = determinePass();
+        this.pass = pass;
 
         myScanner.close();
     }
 
+    /**
+     * @return true if the percentage grade is greater of  equal to the PASSING_GRADE
+     */
+    private boolean determinePass()
+    {
+        return percentageGrade >= PASSING_GRADE;
+    }
 
+    /**
+     * @param testString is the string that is to be checked for whether it is a double
+     * @return the parsed test string as a double
+     * @throws IllegalArgumentException is thrown if the test string is not parsable as a double
+     */
+    private double checkGrade(final String testString) throws IllegalArgumentException
+    {
+        try
+        {
+            return Double.parseDouble(testString);
+        } catch(IllegalArgumentException e)
+        {
+            throw new IllegalArgumentException("invalid percentage grade");
+        }
+    }
+
+    /**
+     * @param testString is the string that is to be checked for whether it consists of a four-digit int
+     * @return the parsed test string as a four-digit int
+     * @throws IllegalArgumentException is thrown if the test string is not parsable as a four-digit int
+     */
+    private int checkBirthYear(final String testString)
+    {
+        final int yearLength = 4;
+
+        if(testString != null && testString.length() == yearLength)
+        {
+            if(areAllDigits(testString))
+            {
+                return Integer.parseInt(testString);
+            }
+        }
+        throw new IllegalArgumentException("Invalid value: not all characters are digits.");
+    }
+
+    /**
+     * @param testString is the test string to be checked for whether it consists of all digits
+     * @return true if all characters of the test string are digits, else false
+     */
+    private boolean areAllDigits(final String testString)
+    {
+        char[] characters = testString.toCharArray();
+
+        for(char character : characters)
+        {
+            if(!Character.isDigit(character))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * @param testString is the string that is to be checked for whether all of its characters are all digits
+     * @return the parsed test string as an int
+     */
+    private int checkDigits(final String testString)
+    {
+        char[] characters = testString.toCharArray();
+
+        for(char character : characters)
+        {
+            if(!Character.isDigit(character))
+            {
+                throw new IllegalArgumentException("Invalid value: not all characters are digits.");
+            }
+        }
+        return Integer.parseInt(testString);
+    }
+
+    /**
+     * @param name is either the first or last name of the student
+     * @return the name
+     */
+    private String checkName(final String name)
+    {
+        if(name != null)
+        {
+            if(isLetter(name))
+            {
+                return name;
+            }
+        }
+        throw new IllegalArgumentException("Invalid name input.");
+    }
+
+    /**
+     * @param testString is the string that is to be checked for whether its characters
+     *                   contain only letters or an apostrophe
+     * @return the parsed test string as an int
+     */
+    private boolean isLetter(final String testString)
+    {
+        final char[] characters;
+        final int    apostropheLimit;
+
+        int count = 0;
+        apostropheLimit = 1;
+
+        characters = testString.toCharArray();
+
+        for(char character : characters)
+        {
+            if(!Character.isLetter(character) && character != '\'')
+            {
+                return false;
+            }
+            if(character == '\'')
+            {
+                count++;
+            }
+            if(count > apostropheLimit)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public String toString()
+    {
+        return this.firstName + " " + this.lastName + "\nid: " + this.id
+                + "\nyear of birth: " + this.birthYear
+                + "\npercentage grade: " + this.percentageGrade + "%"
+                + "\npassed: " + pass;
+    }
+
+    /**
+     * This is the entry point of the program.
+     * @param args are the command line arguments (unused)
+     */
+    public static void main(final String[] args)
+    {
+        Student student;
+        student = new Student();
+
+        System.out.println(student);
+    }
 }
